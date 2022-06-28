01f053cda5001d5631e1bc9c13adf59b5cebc2b9
Douglas Lombard <douglaslombard29@gmail.com>
2022-06-28 13:58:19 +0200

    Finished the basic logic for the battleship game with clear alerts

--- script.js
+++ script.js
@@ -8,19 +8,28 @@ let guess;
 let numberOfHits = 0;
 let numberOfGuesses = 0;
 let isSunk = false;
+let stats = "";
 
 while (isSunk == false) {
   guess = Number(prompt(`Ready, Aim, Fire! (Pick a number between 0-6)`));
   if (guess < 0 || guess > 6) {
-    console.log(`Please enter a valid cell number!`);
+    alert(`Please enter a valid cell number!`);
   } else {
     numberOfGuesses++;
     if (guess === location1 || guess === location2 || guess === location3) {
+      alert("HIT!");
       numberOfHits++;
       if (numberOfHits === 3) {
         isSunk = true;
         alert("You sank my battleship!");
       }
+    } else {
+      alert("MISS");
     }
   }
 }
+
+stats = `You took ${numberOfGuesses} guesses to sink the battleship, which means your shooting accruracy was ${
+  3 / numberOfGuesses
+}`;
+alert(stats);
